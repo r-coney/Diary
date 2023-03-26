d58d2dff88dac8716b4fd96e778ee945721cd26f
rstar <rstar0823@icloud.com>
2023-03-26 12:24:19 +0900

    add: Diary Idが無効な場合にthrowする例外クラスを追加

---
+++ src/domain/DiaryApp/Exceptions/Diary/InvalidIdException.php
@@ -0,0 +1,12 @@
+<?php
+namespace Domain\DiaryApp\Exceptions\Diary;
+
+use Domain\DiaryApp\Exceptions\DomainException;
+
+class InvalidIdException extends DomainException
+{
+    public function handle(): void
+    {
+        
+    }
+}
