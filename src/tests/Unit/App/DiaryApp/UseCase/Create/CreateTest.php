e858b8e3d92dbf08857521c213459bb8dff886db
rstar <rstar0823@icloud.com>
2023-04-02 16:56:57 +0900

    add: InputDataのUnitテストを追加

---
+++ src/tests/Unit/App/DiaryApp/UseCase/Create/CreateTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit\App\DiaryApp\UseCase\Create;
+
+use Tests\TestCase;
+
+class CreateTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
