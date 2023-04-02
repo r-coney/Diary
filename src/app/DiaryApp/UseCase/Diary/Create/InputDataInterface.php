fdfafbd27235202233f64f19f9e6c385a67762d3
rstar <rstar0823@icloud.com>
2023-04-02 16:59:35 +0900

    add: Create/InputDataを追加

---
+++ src/app/DiaryApp/UseCase/Diary/Create/InputDataInterface.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\DiaryApp\UseCase\Diary\Create;
+
+interface InputDataInterface
+{
+    /**
+     * 入力されたタイトルを取得
+     *
+     * @return string
+     */
+    public function title(): string;
+
+    /**
+     * 入力された本文を取得
+     *
+     * @return string|null
+     */
+    public function content(): ?string;
+
+    /**
+     * 選択されたメインカテゴリーを取得
+     *
+     * @return int
+     */
+    public function mainCategoryId(): int;
+
+    /**
+     * 選択されたサブカテゴリーを取得
+     *
+     * @return int|null
+     */
+    public function subCategoryId(): ?int;
+}
