cfec739605c2791ff11105309945b68defbb3dc9
rstar <rstar0823@icloud.com>
2023-04-09 06:36:00 +0900

    add: QueryServiceのinterfaceを追加

---
+++ src/app/DiaryApp/UseCase/Diary/QueryServiceInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Database\Eloquent\Collection;
+
+interface QueryServiceInterface
+{
+    /**
+     * 日記一覧を取得
+     * @param GetListCommand $command
+     * @return Collection
+     */
+    public function getList(GetListCommand $command): Collection;
+}
