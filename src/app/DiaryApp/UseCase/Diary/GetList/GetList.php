9fc9080572ccaed635d7eb2483f8d9ead3a37505
rstar <rstar0823@icloud.com>
2023-04-08 16:21:45 +0900

    add: GetListアプリケーションサービスクラスを追加

---
+++ src/app/DiaryApp/UseCase/Diary/GetList/GetList.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\DiaryApp\UseCase\Diary\GetList;
+
+use App\DiaryApp\UseCase\Diary\GetList\GetListInterface;
+
+class GetList implements GetListInterface
+{
+
+}
--- src/app/Providers/DiaryApp/ApplicationServiceProvider.php
+++ src/app/Providers/DiaryApp/ApplicationServiceProvider.php
@@ -2,11 +2,13 @@
 
 namespace App\Providers\DiaryApp;
 
-use App\DiaryApp\UseCase\Diary\Create\Create;
-use App\DiaryApp\UseCase\Diary\Create\CreateCommand;
-use App\DiaryApp\UseCase\Diary\Create\CreateCommandInterface;
-use App\DiaryApp\UseCase\Diary\Create\CreateInterface;
 use Illuminate\Support\ServiceProvider;
+use App\DiaryApp\UseCase\Diary\Create\Create;
+use App\DiaryApp\UseCase\Diary\GetList\GetList;
+use App\DiaryApp\UseCase\Diary\Create\CreateCommand;
+use App\DiaryApp\UseCase\Diary\Create\CreateInterface;
+use App\DiaryApp\UseCase\Diary\GetList\GetListInterface;
+use App\DiaryApp\UseCase\Diary\Create\CreateCommandInterface;
 
 class ApplicationServiceProvider extends ServiceProvider
 {
@@ -17,6 +19,7 @@ class ApplicationServiceProvider extends ServiceProvider
     {
         $this->app->bind(CreateInterface::class, Create::class);
         $this->app->bind(CreateCommandInterface::class, CreateCommand::class);
+        $this->app->bind(GetListInterface::class, GetList::class);
     }
 
     /**
