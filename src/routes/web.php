b4c8bc064e69d658f8eadc1d7bffcb8facb0ea82
rstar <rstar0823@icloud.com>
2023-04-08 15:49:35 +0900

    chore: ユーザー認証機能のライブラリを追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,9 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('/diaries')->name('diaryApp.')->namespace('App\Http\Controllers\DiaryApp')->group(function () {
-    Route::name('diary.')->namespace('Diary')->group(function () {
-        Route::get('/create', 'Create')->name('create');
-        Route::post('/', 'Store')->name('store');
-    });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
