d4279cb4a1297f509d9af0400dbb538de8e06c5b
rstar <rstar0823@icloud.com>
2023-04-13 06:35:15 +0900

    add: 日記詳細取得失敗時に投げる例外クラスを追加

---
+++ src/app/Exceptions/DiaryApp/Diary/UseCase/DiaryNotFoundException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Exceptions\DiaryApp\Diary\UseCase;
+
+use App\Exceptions\ApplicationException;
+
+class DiaryNotFoundException extends ApplicationException
+{
+    public function render(): void
+    {
+        
+    }
+}
