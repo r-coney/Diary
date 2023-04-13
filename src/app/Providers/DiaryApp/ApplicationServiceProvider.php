f96e7143283e88fa89f85255880b91b238f41f04
rstar <rstar0823@icloud.com>
2023-04-13 10:06:50 +0900

    feat: 日記詳細表示機能を実装

--- src/app/Providers/DiaryApp/ApplicationServiceProvider.php
+++ src/app/Providers/DiaryApp/ApplicationServiceProvider.php
@@ -5,11 +5,13 @@ namespace App\Providers\DiaryApp;
 use Illuminate\Support\ServiceProvider;
 use App\DiaryApp\UseCase\Diary\Create\Create;
 use App\DiaryApp\UseCase\Diary\GetList\GetList;
+use App\DiaryApp\UseCase\Diary\GetDetail\GetDetail;
 use App\DiaryApp\UseCase\Diary\Create\CreateCommand;
 use App\DiaryApp\UseCase\Diary\Create\CreateInterface;
 use App\DiaryApp\UseCase\Diary\GetList\GetListInterface;
-use App\DiaryApp\UseCase\Diary\Create\CreateCommandInterface;
 use App\DiaryApp\UseCase\Diary\GetList\DiaryListQueryData;
+use App\DiaryApp\UseCase\Diary\GetDetail\GetDetailInterface;
+use App\DiaryApp\UseCase\Diary\Create\CreateCommandInterface;
 use App\DiaryApp\UseCase\Diary\GetList\DiaryListQueryDataInterface;
 
 class ApplicationServiceProvider extends ServiceProvider
@@ -23,6 +25,7 @@ class ApplicationServiceProvider extends ServiceProvider
         $this->app->bind(CreateCommandInterface::class, CreateCommand::class);
         $this->app->bind(GetListInterface::class, GetList::class);
         $this->app->bind(DiaryListQueryDataInterface::class, DiaryListQueryData::class);
+        $this->app->bind(GetDetailInterface::class, GetDetail::class);
     }
 
     /**
---
+++ src/resources/views/diary_app/diary/detail.blade.php
@@ -0,0 +1,8 @@
+<h2>日記詳細</h2>
+
+{{ $diaryData->title }}
+{{ $diaryData->content }}
+{{ $diaryData->mainCategoryName }}
+{{ $diaryData->subCategoryName }}
+{{ $diaryData->createdAt }}
+{{ $diaryData->updatedAt }}
