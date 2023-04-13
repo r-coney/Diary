1a0a197ce7f2ae150c500f1a690bd0a1b9de42e2
rstar <rstar0823@icloud.com>
2023-04-13 09:35:22 +0900

    feat: 日記詳細取得アプリケーションサービスを実装

--- src/app/DiaryApp/UseCase/Diary/GetDetail/DiaryData.php
+++ src/app/DiaryApp/UseCase/Diary/GetDetail/DiaryData.php
@@ -1,10 +1,28 @@
 <?php
 namespace App\DiaryApp\UseCase\Diary\GetDetail;
 
+use Domain\DiaryApp\Models\Diary\Diary;
+
 class DiaryData
 {
-    public function __construct(array $diaryData)
-    {
+    public int $id;
+    public int $userId;
+    public string $mainCategoryName;
+    public ?string $subCategoryName;
+    public string $title;
+    public string $content;
+    public string $createdAt;
+    public ?string $updatedAt;
 
+    public function __construct(Diary $diaryData)
+    {
+        $this->id = $diaryData->id();
+        $this->userId = $diaryData->userId();
+        $this->mainCategoryName = $diaryData->mainCategoryName();
+        $this->subCategoryName = $diaryData->subCategoryName();
+        $this->title = $diaryData->title();
+        $this->content = $diaryData->content();
+        $this->createdAt = $diaryData->createdAt();
+        $this->updatedAt = $diaryData->updatedAt();
     }
 }
--- src/app/DiaryApp/UseCase/Diary/GetDetail/GetDetail.php
+++ src/app/DiaryApp/UseCase/Diary/GetDetail/GetDetail.php
@@ -1,13 +1,27 @@
 <?php
 namespace App\DiaryApp\UseCase\Diary\GetDetail;
 
+use Domain\DiaryApp\Models\Diary\Id;
+use App\Exceptions\DiaryApp\Diary\UseCase\DiaryNotFoundException;
+use Domain\DiaryApp\Models\Diary\RepositoryInterface as DiaryRepositoryInterface;
+
 class GetDetail
 {
-    public function __invoke(Id $id): DiaryDetailedData
+    private DiaryRepositoryInterface $diaryRepository;
+
+    public function __construct(DiaryRepositoryInterface $diaryRepository)
     {
-        // categoryをDiaryEntityに持たせる
-        $diary = $this->diaryRepository->findById($id);
-        $diaryDetailedData = new DiaryDetailedData($diary);
-        return $diaryDetailedData;
+        $this->diaryRepository = $diaryRepository;
+    }
+
+    public function __invoke(Id $id): DiaryData
+    {
+        $diary = $this->diaryRepository->find($id);
+
+        if (is_null($diary)) {
+            throw new DiaryNotFoundException('日記が見つかりませんでした。');
+        }
+
+        return new DiaryData($diary);
     }
 }
