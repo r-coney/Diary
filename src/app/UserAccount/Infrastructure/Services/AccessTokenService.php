0a6506ec9150f3010e0548beff3bf61693ed4475
rstar <rstar0823@icloud.com>
2023-07-08 16:18:13 +0900

    add: AccessTokenServiceを追加

---
+++ src/app/UserAccount/Infrastructure/Services/AccessTokenService.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\UserAccount\Infrastructure\Services;
+
+class AccessTokenService implements AccessTokenServiceInterface
+{
+
+}
---
+++ src/app/UserAccount/Infrastructure/Services/AccessTokenServiceInterface.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\UserAccount\Infrastructure\Services;
+
+interface AccessTokenServiceInterface
+{
+
+}
