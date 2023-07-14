4ebf2a5d59322e3027b393d4142c8fdec4c3f51c
rstar <rstar0823@icloud.com>
2023-07-14 09:29:39 +0900

    add: VerifyTokenCommandのUnitテストを追加

---
+++ src/tests/Unit/App/UserAccount/UseCase/User/VefifyAccessToken/VerifyTokenCommandTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit\App\UserAccount\UseCase\User\VefifyAccessToken;
+
+use PHPUnit\Framework\TestCase;
+
+class VerifyTokenCommandTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
