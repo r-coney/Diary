6a90593e34345bac6644bb2ecfbb34fef29a58ff
rstar <rstar0823@icloud.com>
2023-07-17 08:52:46 +0900

    docs: docコメントを修正

--- src/app/UserAccount/UseCase/User/VerifyAccessToken/VerifyAccessTokenInterface.php
+++ src/app/UserAccount/UseCase/User/VerifyAccessToken/VerifyAccessTokenInterface.php
@@ -5,6 +5,7 @@ interface VerifyAccessTokenInterface
 {
     /**
      * アクセストークンが有効化か判定
+     * @throws AuthenticationException
      * @return bool
      */
     public function __invoke(VerifyTokenCommand $command): bool;
