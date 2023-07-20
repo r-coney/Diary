592eba8202af8b0f28bf37cbcecf101685d5a3db
rstar <rstar0823@icloud.com>
2023-07-20 09:51:33 +0900

    add: userAccoutに使用する定数クラスを追加

---
+++ src/app/UserAccount/Consts/UserConst.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\UserAccount\Consts;
+
+class UserConst
+{
+    const INPUT_NAME = 'name';
+    const INPUT_EMAIL = 'email';
+    const INPUT_PASSWORD = 'password';
+    const INPUT_PASSWORD_CONFIRMATION = 'passwordConfirmation';
+}
